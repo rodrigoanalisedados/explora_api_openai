79dd58a7a56e0a9377260f2026d330abf4901e92
Rodrigo Xavier dos Santos <rodrigo.analise.dados@gmail.com>
2025-06-22 15:20:12 -0300

    Carrega arquivo de transações

--- analisador_transacoes.py
+++ analisador_transacoes.py
@@ -4,6 +4,7 @@ import os
 import json
 
 load_dotenv()
+
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4.1-mini"
 
@@ -23,3 +24,64 @@ def salva(nome_do_arquivo, conteudo):
             arquivo.write(conteudo)
     except IOError as e:
         print(f"Erro ao salvar arquivo: {e}")
+
+# código omitido
+
+def analisar_transacao(lista_transacoes):
+    print("1. Executando a análise de transação")
+    
+    prompt_sistema = """
+    Analise as transações financeiras a seguir e identifique se cada uma delas é uma "Possível Fraude" ou deve ser "Aprovada". 
+    Adicione um atributo "Status" com um dos valores: "Possível Fraude" ou "Aprovado".
+
+    Cada nova transação deve ser inserida dentro da lista do JSON.
+
+    # Possíveis indicações de fraude
+    - Transações com valores muito discrepantes
+    - Transações que ocorrem em locais muito distantes um do outro
+    
+        Adote o formato de resposta abaixo para compor sua resposta.
+        
+    # Formato Saída 
+    {
+        "transacoes": [
+            {
+            "id": "id",
+            "tipo": "crédito ou débito",
+            "estabelecimento": "nome do estabelecimento",
+            "horário": "horário da transação",
+            "valor": "R$XX,XX",
+            "nome_produto": "nome do produto",
+            "localização": "cidade - estado (País)"
+            "status": ""
+            },
+        ]
+    } 
+    """
+
+    lista_mensagens = [
+        {
+            "role": "system",
+            "content": prompt_sistema
+        },
+        {
+                "role": "user",
+                "content": f"Considere o CSV abaixo, onde cada linha é uma transação diferente: {lista_transacoes}. Sua resposta deve adotar o #Formato de Resposta (apenas um json sem outros comentários)"
+        }
+    ]
+
+    resposta = cliente.chat.completions.create(
+        messages = lista_mensagens,
+        model = modelo,
+        temperature = 0
+    )
+
+    conteudo = resposta.choices[0].message.content.replace("'", '"')
+    print("\Conteúdo:", conteudo)
+    json_resultado = json.loads(conteudo)
+    print("\nJSON:", json_resultado)
+    return json_resultado
+
+lista_de_transacoes = carrega("dados/transacoes.csv")
+analisar_transacao(lista_de_transacoes)
+
